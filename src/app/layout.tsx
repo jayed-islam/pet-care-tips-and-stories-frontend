5154dd6aaec0f2fe13eb51453e854a6c60860897
jayed-phero <108389914+jayed-islam@users.noreply.github.com>
2024-10-07 18:24:48 +0600

    updae redme file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,26 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
+import localFont from "next/font/local";
 import "./globals.css";
 import { ReduxProvider } from "@/redux/ReduxProvider";
 import { Toaster } from "react-hot-toast";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+const robotoBold = localFont({
+  src: "./fonts/Roboto-Bold.ttf",
+  variable: "--font-roboto-bold",
+  weight: "700", // Weight for Bold
+});
 
-// const robotoBold = localFont({
-//   src: "./fonts/Roboto-Bold.ttf",
-//   variable: "--font-roboto-bold",
-//   weight: "700", // Weight for Bold
-// });
+const robotoMedium = localFont({
+  src: "./fonts/Roboto-Medium.ttf",
+  variable: "--font-roboto-medium",
+  weight: "500", // Weight for Medium
+});
 
-// const robotoMedium = localFont({
-//   src: "./fonts/Roboto-Medium.ttf",
-//   variable: "--font-roboto-medium",
-//   weight: "500", // Weight for Medium
-// });
-
-// const robotoRegular = localFont({
-//   src: "./fonts/Roboto-Regular.ttf",
-//   variable: "--font-roboto-regular",
-//   weight: "400", // Weight for Regular
-// });
-
-// const oswaldRegular = localFont({
-//   src: "./fonts/Oswald-Bold.ttf",
-//   variable: "--font-roboto-regular",
-//   weight: "400", // Weight for Regular
-// });
+const robotoRegular = localFont({
+  src: "./fonts/Roboto-Regular.ttf",
+  variable: "--font-roboto-regular",
+  weight: "400", // Weight for Regular
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -52,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-      // className={`${robotoBold.variable} ${robotoMedium.variable} ${robotoRegular.variable} ${oswaldRegular.variable} antialiased`}
+        className={`${robotoBold.variable} ${robotoMedium.variable} ${robotoRegular.variable} antialiased`}
       >
         <ReduxProvider>
           <Toaster />
